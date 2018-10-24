46382fdc1143d6daafb504101dfbfc17a24e58a3
Peterzxcvbnm <43370082+Peterzxcvbnm@users.noreply.github.com>
2018-10-24 10:43:31 +0200

    ServerConnection
    Class to be able to connect to the server.

---
+++ GLO18_Client/src/client/Link/ServerConnection.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client.Link;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Peterzxcvbnm
+ * @since 2018-10-24
+ * @version 1.0
+ */
+public class ServerConnection {
+    
+    private Socket socket;
+    private Scanner scanner;
+
+    private ServerConnection(String serverAddress, int serverPort) throws Exception {
+        this.socket = new Socket(serverAddress, serverPort);
+        this.scanner = new Scanner(System.in);
+    }
+
+    private void sendMessage(String message) throws IOException {
+        PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+        out.println(message);
+        out.flush();
+    }
+    
+    private String receiveMessage() throws IOException{
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(this.socket.getInputStream()));
+        return in.readLine();
+    }
+
+//    public static void main(String[] args) throws Exception {
+//        ServerConnection client = new ServerConnection(
+//                "10.126.98.185",
+//                Integer.parseInt("2345"));
+//
+//        System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
+//        
+//        client.sendMessage("It works!!");
+//        System.out.println(client.receiveMessage());
+//
+//        while (true);
+//    }
+}
