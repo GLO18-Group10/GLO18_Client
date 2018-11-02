fdbf5663f530e896b8a92682a8dc42aef43f739e
Snowball44 <32239917+Snowball44@users.noreply.github.com>
2018-11-02 14:46:11 +0100

    Temp fix to
    temp fx

--- GLO18_Client/src/client/GUI/adminController.java
+++ GLO18_Client/src/client/GUI/adminController.java
@@ -6,11 +6,14 @@
 package client.GUI;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -34,36 +37,63 @@ public class adminController implements Initializable {
     @FXML TextField LastnameField;
     @FXML TextField CPRField;
     @FXML TextField EmailField;
-    TextField BirtdayField;
-    @FXML TextField PhoneField;
-    @FXML
-    private TextField BirthdayField;
+    @FXML 
+    TextField PhoneField;
+    ArrayList<TextField> textFields;
     @FXML
     private TextField AddressField;
     @FXML
     private TextField PasswordField;
+    @FXML
+    private DatePicker birthdayFieldDatePicker;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        textFields = new ArrayList<>();
+        textFields.add(CPRField);
+        textFields.add(FirstNameField);
+        textFields.add(LastnameField);
+        textFields.add(PhoneField);
+        textFields.add(AddressField);
+        textFields.add(EmailField);
+        textFields.add(PasswordField);
     }    
     
     @FXML
     private void CreateButtonHandler(ActionEvent event) {
+        if(isEmpty(textFields) || birthdayFieldDatePicker.getValue()== null){
+            System.out.println("TOM TEST I CONTROLLER");
+        }
+        else{
         String ID = "C" + CPRField.getText();
         String name = FirstNameField.getText() + " " + LastnameField.getText();
-        String birthday = BirthdayField.getText();
+        String birthdayTest = birthdayFieldDatePicker.getValue().toString();
+        System.out.println(birthdayTest);
         String phoneNumber = PhoneField.getText();
         String address = AddressField.getText();
         String email = EmailField.getText();
         String password = PasswordField.getText();
-        System.out.println(createCustomer(ID, name, birthday, phoneNumber, address, email, password));
+        System.out.println(createCustomer(ID, name, birthdayTest, phoneNumber, address, email, password));
+        }
     }
     
     private String createCustomer(String ID, String name, String birthday, String phoneNumber, String address, String email, String password){
        return guiRun.getInstance().toProtocol07(ID, name, birthday, phoneNumber, address, email, password);
+    }
+    
+    private boolean isEmpty(ArrayList<TextField> textFieldArray){
+        boolean isEmpty = false;
+        for (TextField textField : textFieldArray) {
+            if(textField.getText().trim().isEmpty()){
+                System.out.println(textField.toString());
+                String[] textFieldEmpty = textField.toString().split(",");
+                System.out.println(textFieldEmpty[0].substring(13) + " IS EMPTY");
+                isEmpty = true;
+            }
+        }
+        return isEmpty;
     }
                 
     
