ad3d30e2f0c98654f242ce515b41e55e14990df3
Peterzxcvbnm <43370082+Peterzxcvbnm@users.noreply.github.com>
2018-11-30 14:48:39 +0100

    Proceed button clears error message

--- GLO18_Client/src/client/GUI/CustomerController.java
+++ GLO18_Client/src/client/GUI/CustomerController.java
@@ -40,7 +40,7 @@ import javafx.stage.Stage;
  * @author antonio
  */
 public class CustomerController implements Initializable {
-
+    
     IGUI gui;
     ILogic logic;
     @FXML
@@ -137,24 +137,24 @@ public class CustomerController implements Initializable {
     private Button CancelTransactionButton;
     @FXML
     private PasswordField ConfirmPasswordField;
-
+    
     public CustomerController() {
     }
-
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         gui = GUIrun.getInstance();
         logic = GUIrun.getLogic();
     }
-
+    
     @FXML
     private void handleButtonAction(javafx.scene.input.MouseEvent event) throws IOException {
-
+        
         if (event.getSource() == TransferButton) {
             clearPanes();
             NewTransferAnchorPane.toFront();
             NewTransferAnchorPane.setVisible(true);
-
+            
             if (TransactionBankIDChoiceBox.getItems().isEmpty()) {
                 String bankid[] = logic.getCustomer().getBankID().split(";");
                 for (int i = 0; i < bankid.length; i++) {
@@ -165,7 +165,7 @@ public class CustomerController implements Initializable {
             clearPanes();
             AccountsAnchorPane.toFront();
             AccountsAnchorPane.setVisible(true);
-
+            
             if (AccountsDropdown.getItems().isEmpty()) {
                 String bankid[] = logic.getCustomer().getBankID().split(";");
                 for (int i = 0; i < bankid.length; i++) {
@@ -214,22 +214,22 @@ public class CustomerController implements Initializable {
             }
         }
     }
-
+    
     private void setAccountBalance() {
         AccountBalanceLabel.setText(logic.getAccountBalance(AccountsDropdown.getValue()) + " DKK");
     }
-
+    
     private void clearPanes() {
         NewTransferAnchorPane.setVisible(false);
         AccountsAnchorPane.setVisible(false);
         AnchorPane3.setVisible(false);
         ProfileAnchor.setVisible(false);
     }
-
+    
     private String storeCustomerInfo(String name, String phoneNo, String address, String email) {
         return logic.toProtocol03(name, phoneNo, address, email);
     }
-
+    
     @FXML
     private void storeCustomerInfoButtonHandler(javafx.event.ActionEvent event) {
         String firstName = NameField.getText();
@@ -261,7 +261,7 @@ public class CustomerController implements Initializable {
             SaveButton.setVisible(false);
         }
     }
-
+    
     @FXML
     private void EditEnableButtonHandler(javafx.event.ActionEvent event) {
         NameField.setDisable(false);
@@ -272,7 +272,7 @@ public class CustomerController implements Initializable {
         CancelEditButton.setVisible(true);
         SaveButton.setVisible(true);
     }
-
+    
     @FXML
     private void CancelEditButtonHandler(javafx.event.ActionEvent event) {
         CancelEditButton.setVisible(false);
@@ -284,7 +284,7 @@ public class CustomerController implements Initializable {
         EmailField.setDisable(true);
         RestoreInfoInFields();
     }
-
+    
     private void RestoreInfoInFields() {
         NameField.setText(logic.getCustomer().getName().split(" ")[0]);
         LastNameField.setText(logic.getCustomer().getName().split(" ")[1]);
@@ -292,9 +292,10 @@ public class CustomerController implements Initializable {
         AddressField.setText(logic.getCustomer().getAddress());
         EmailField.setText(logic.getCustomer().getEmail());
     }
-
+    
     @FXML
     private void proceedTransfer() {
+        TransactionOverallMessageLabel.setText("");
         AmountErrorLabel.setText("");
         AccountErrorLabel.setText("");
         MessageErrorLabel.setText("");
@@ -330,7 +331,7 @@ public class CustomerController implements Initializable {
             }
         }
     }
-
+    
     @FXML
     private void CancelTransfer(ActionEvent event
     ) {
@@ -339,7 +340,7 @@ public class CustomerController implements Initializable {
         ConfirmPasswordField.setVisible(false);
         defreezeInputTransaction();
     }
-
+    
     @FXML
     private void transfer(ActionEvent event) {
         AmountErrorLabel.setText("");
@@ -385,13 +386,13 @@ public class CustomerController implements Initializable {
         String accountID = AccountsDropdown.getValue();
         TransactionHistoryListView.getItems().clear();
         String data[] = logic.getTransactionHistory(accountID).split(";");
-
+        
         for (int i = data.length - 1; i >= 0; i--) {
             TransactionHistoryListView.getItems().add(data[i]);
-
+            
         }
     }
-
+    
     private boolean containsInvalidInput(String stringToCheck) {
         char[] charArrayToCheck = stringToCheck.toCharArray();
         for (char c : charArrayToCheck) {
@@ -401,7 +402,7 @@ public class CustomerController implements Initializable {
         }
         return false;
     }
-
+    
     @FXML
     private void cleanInputFields(ActionEvent event) {
         AccountField.clear();
@@ -414,7 +415,7 @@ public class CustomerController implements Initializable {
         TransactionOverallMessageLabel.setText("");
         ConfirmPasswordField.setText("");
     }
-
+    
     @FXML
     private void changePassword() {
         //Check for illegal characters and compare the two passwords
@@ -469,7 +470,7 @@ public class CustomerController implements Initializable {
         newPasswordField.clear();
         repeatPasswordField.clear();
     }
-
+    
     private String makeInt(String text) {
         int commaPos = text.indexOf(",");
         if (commaPos == -1) {
@@ -499,7 +500,7 @@ public class CustomerController implements Initializable {
             return text;
         }
     }
-
+    
     private boolean checkAmount(String amount) {
         char[] charArrayToCheck = amount.toCharArray();
         int commaCount = 0;
@@ -517,14 +518,14 @@ public class CustomerController implements Initializable {
         }
         return false;
     }
-
+    
     private void freezeInputTransaction() {
         AmountField.setEditable(false);
         AccountField.setEditable(false);
         RegField.setEditable(false);
         MessageArea.setEditable(false);
     }
-
+    
     private void defreezeInputTransaction() {
         AmountField.setEditable(true);
         AccountField.setEditable(true);
